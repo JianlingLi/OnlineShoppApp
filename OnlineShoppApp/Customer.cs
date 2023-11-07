f9560636884f693507d97766cfc7d94e7e4ba73b
JianlingLi <kearalee@hotmail.com>
2023-11-07 10:38:59 +0100

    Add project files.

---
+++ OnlineShoppApp/Customer.cs
@@ -0,0 +1,29 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Customer {
+
+    public Customer() {
+    }
+
+    public string UserID;
+
+    public string Name;
+
+    public string BillAdd;
+
+    public string DeliverAdd;
+
+    public int TelNum;
+
+    public string Email;
+
+    public void AddCustomer() {
+        // TODO implement here
+    }
+
+
+}
